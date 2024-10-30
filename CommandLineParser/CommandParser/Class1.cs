629a5be437dc98eb8b148971b46b50aab2ae541c
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2024-10-30 15:04:18 +0000

    Initial Commit

---
+++ CommandLineParser/CommandParser/Class1.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CommandParser
+{
+    public class CommandParser
+    {
+        public static SchemaStruct parse<SchemaStruct>(string[] args) where SchemaStruct : struct
+        {
+            Type type = typeof(SchemaStruct);
+            
+            SchemaStruct returnType = new SchemaStruct();
+
+            Dictionary<string, string> fields = new Dictionary<string, string>();
+            string key = null;
+            foreach (string arg in args)
+            {
+                if (arg.StartsWith("--")) {
+                    key = arg.TrimStart('-');
+                }
+                else
+                {
+                    fields.Add(key, arg);
+                }
+            }
+
+            foreach (FieldInfo field in type.GetFields())
+            {
+                Type x = field.FieldType;
+                string name = field.Name;
+
+                if (field == null) { continue; }
+                if (fields.ContainsKey(name))
+                {
+                    // TODO: make sure this cannot fail
+                    var ValueForField = Convert.ChangeType(fields[name], x);
+                    field.SetValueDirect(__makeref(returnType), ValueForField);
+                }
+            }
+
+            return returnType;
+        }
+    }
+}
---
+++ CommandLineParser/TestApp/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+arguments a = CommandParser.CommandParser.parse<arguments>(args);
+
+Console.WriteLine("###########################################################");
+Console.WriteLine($"{a.id}, {a.id.GetType()}");
+Console.WriteLine($"{a.password}, {a.password.GetType()}");
+Console.WriteLine($"{a.year}, {a.year.GetType()}");
+Console.WriteLine($"{a.month}, {a.month.GetType()}");
+
+struct arguments
+{
+    public string id;
+    public string password;
+    public int year;
+    public int month;
+
+    public arguments(string id, string password, int year, int month)
+    {
+        this.id = id;
+        this.password = password;
+        this.year = year;
+        this.month = month;
+    }
+
+    public arguments() : this("default", "default", 200, 1)
+    {
+    }
+}
